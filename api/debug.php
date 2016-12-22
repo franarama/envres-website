9a25c047735d3e391a83aa00aa979e8eda190717
Fran Ramunno <franramunno@gmail.com>
2016-12-22 11:38:58 -0800

    Create debug.php

---
+++ api/debug.php
@@ -0,0 +1,23 @@
+<HTML>
+<HEAD>
+<TITLE>Debugging info</TITLE>
+</HEAD>
+<BODY>
+
+<u>POST ($_POST) variables</u><br>
+<pre>
+<?php
+print_r($_POST);
+
+?>
+</pre>
+<br>
+<hr>
+<br>
+<u>GET ($_GET) variables</u><br>
+<pre>
+<?php
+print_r($_GET);
+?>
+</pre>
+</BODY>
